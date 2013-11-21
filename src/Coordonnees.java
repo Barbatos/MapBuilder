a4b7c685da92787788266dfb9a5541106d97bd0e
Barbatos <barbatos@urbanterror.info>
2013-11-21 08:37:09 +0100

    Ajout de la classe coordonnees

---
+++ src/Coordonnees.java
@@ -0,0 +1,11 @@
+
+public abstract class Coordonnees {
+	private int x;
+	private int y;
+	
+	public abstract int getX();
+	public abstract int getY();
+	
+	public abstract void setX(int _x);
+	public abstract void setY(int _y);
+}

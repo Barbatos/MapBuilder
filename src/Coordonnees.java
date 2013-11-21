329617bbc2bf76ccdb986cc428ae8185ee585604
battueur <adrien.demoget@gmail.com>
2013-11-21 09:08:59 +0100

    Ajout d'un constructeur

--- src/Coordonnees.java
+++ src/Coordonnees.java
@@ -2,6 +2,16 @@
 public class Coordonnees {
 	private int x;
 	private int y;
+
+	public Coordonnees() {
+		this.x = 0;
+		this.y = 0;
+	}
+
+	public Coordonnees(int _x, int _y) {
+		this.x = _x;
+		this.y = _y;
+	}
 	
 	public int getX(){
 		return this.x;

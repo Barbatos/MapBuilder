d861f109d7b2790307658048291d6ab5cc58f3f0
battueur <adrien.demoget@gmail.com>
2013-11-21 11:21:51 +0100

    Ajout méthodes

--- src/Ligne.java
+++ src/Ligne.java
@@ -1,7 +1,8 @@
-
-public class Ligne {
+import java.util.*;
+public class Ligne extends Station{
 	private int id;
 	private String nom;
+	private Vector<Station> listeStations;
 	
 	public Ligne(){
 		this.id = -1;
@@ -27,5 +28,33 @@ public class Ligne {
 	
 	public void setNom(String _nom){
 		this.nom = _nom;
+	}
+
+	public void setStationDepart(Station station) {
+		listeStations.add(0, station);
+	}
+
+	public void setStationArrivee(Station station) {
+		int taille = listeStations.size();
+		listeStations.add(taille + 1, station);
+	}
+
+	public void insertStation(Station station, int numero) {
+		if (numero < listeStations.size())
+			listeStations.insertElementAt(station, numero);
+		else
+			System.out.println("Erreur d'insertion de station dans la Ligne !");
+	}
+
+	public Station getStation(int numero) {
+		if (numero < listeStations.size())
+			return listeStations.elementAt(numero);
+		else 
+			System.out.println("Erreur d'insertion de station dans la Ligne !");
+			return listeStations.lastElement();
+	}
+
+	public int getNumeroStation(Station station) {
+		return listeStations.indexOf(station);
 	}
 }

da12b127345c6ea97af4fc4ebe3b9f223092a0f4
Bardoll <t.labroue@gmail.com>
2013-11-21 08:51:15 +0100

    Ajout de la classe Intersection

---
+++ src/Intersection.java
@@ -0,0 +1,22 @@
+
+public class Intersection {
+
+	int ID;
+	String Type;
+	
+	public int getID(){
+		return ID;
+	}
+	
+	public String getType(){
+		return Type;
+	}
+	
+	public void setID(int ID){
+		this.ID = ID;
+	}
+	
+	public void setType(String Type){
+		this.Type = Type;
+	}
+}

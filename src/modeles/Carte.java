e9a446b1480afd2cdee17b2b7af42d583b437f0f
battueur <adrien.demoget@gmail.com>
2013-12-05 14:56:21 +0100

    ajout de la méthode permettant de récupérer les stations en dehors d'une zone donnée

--- src/modeles/Carte.java
+++ src/modeles/Carte.java
@@ -9,9 +9,39 @@ import java.util.Vector;
 public class Carte {
 	private Vector<Zone> listeZones = new Vector<Zone>();
 	private Vector<Ligne> listeLignes = new Vector<Ligne>();
-	
+	private Vector<Ville> listeVilles = new Vector<Ville>();
+	private Vector<Station> listeStations = new Vector<Station>();
+
 	public Carte(){
 		
+	}
+	
+	public Carte(Vector<Zone> listeZones, Vector<Ligne> listeLignes, Vector<Ville> listeVilles, Vector<Station> listeStations) {
+		this.listeLignes = listeLignes;
+		this.listeStations = listeStations;
+		this.listeVilles = listeVilles;
+		this.listeZones = listeZones;
+	}
+	
+	Vector<Station> stationsExterieures(Zone zone) {
+		Vector<Station> listeExte = new Vector<Station>();
+		boolean dehors;
+		
+		for(int i = 0; i < listeStations.size(); i++) {
+			dehors = true;
+			
+			for(int j = 0; j < zone.getListeStations().size(); j++) {
+				if(listeStations.elementAt(i) == zone.getStation(j)) {
+					dehors = false;
+				}
+			}
+			
+			if(dehors) {
+				listeExte.add(listeStations.elementAt(i));
+			}
+		}	
+		
+		return listeExte;
 	}
 	
 	public void dessinerCarte(Graphics g){

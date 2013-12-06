7b2c0b4b566ad563e64da900e064cfa1624e232f
battueur <adrien.demoget@gmail.com>
2013-12-06 08:15:23 +0100

    Travail sur l'algorithme de tracé des zones.

--- src/modeles/Ville.java
+++ src/modeles/Ville.java
@@ -1,15 +1,21 @@
 package modeles;
 
+import java.awt.Graphics;
+import java.util.Vector;
+
 public class Ville{
 	private String nom;
-	//private Vector<Segment> listeSegments = new Vector<Segment>();
+	private int x[];
+	private int y[];
 	
 	public Ville(){
 		this.nom = "Pas de nom";
 	}
 	
-	public Ville(String _nom){
+	public Ville(String _nom, int x[], int y[]){
 		this.nom = _nom;
+		this.x = x;
+		this.y = y;
 	}
 	
 	public String getNom(){
@@ -18,5 +24,10 @@ public class Ville{
 	
 	public void setNom(String _nom){
 		this.nom = _nom;
+	}
+	
+	public void dessinerVille(Graphics g) {
+		//il faut faire un polygone à partir des coordonnées de la ville.
+		//la série de points est à encadrer selon l'algorithme de graham ou de jarvis
 	}
 }

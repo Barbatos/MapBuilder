3e859fbc41aa3335c7430bd39762db62806fe338
Charles Duprey <barbatos@urbanterror.info>
2014-01-06 15:10:39 +0100

    Fenêtre d'ajout des données, WIP

--- src/vues/AjoutDonneesPanel.java
+++ src/vues/AjoutDonneesPanel.java
@@ -1,11 +1,14 @@
 package vues;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Graphics;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class AjoutDonneesPanel extends JPanel{
 	private static final long serialVersionUID = 1L;
@@ -29,18 +32,38 @@ public class AjoutDonneesPanel extends JPanel{
 
 		// On empêche le redimensionnement de la fenêtre
 		fenetre.setResizable(false);
-		
+        
 		// Import de la classe Affichage
 		fenetre.setContentPane(this);
 		
 		fenetre.setVisible(true);
+		
+		Container pane = fenetre.getContentPane();
+		
+		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		
+		JButton boutonAjoutVille = new JButton("Ajouter une ville");
+		boutonAjoutVille.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		JButton boutonAjoutStation = new JButton("Ajouter une station");
+		boutonAjoutStation.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		JButton boutonAjoutLigne = new JButton("Ajouter une ligne");
+		boutonAjoutLigne.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        JButton boutonAjoutZone = new JButton("Ajouter une zone");
+        boutonAjoutZone.setAlignmentX(Component.CENTER_ALIGNMENT);
+       
+        pane.add(boutonAjoutVille);
+        pane.add(boutonAjoutStation);
+        pane.add(boutonAjoutLigne);
+        pane.add(boutonAjoutZone);
+        
+        fenetre.setContentPane(pane);
 	}
 	
 	public void paintComponent(Graphics g){
-		JTextField nomStation = new JTextField();
-		
 		super.paintComponent(g);
-		setBackground(new Color(175, 175, 175));
-		
+        setBackground(new Color(175, 175, 175));
 	}
 }

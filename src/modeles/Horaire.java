2c5cbbbc2e0efc536381b921c8478b4a292974d5
Charles Duprey <barbatos@urbanterror.info>
2014-01-07 10:53:49 +0100

    Javadoc des classes Horaire et Ligne (by @soullessoni)

--- src/modeles/Horaire.java
+++ src/modeles/Horaire.java
@@ -3,12 +3,20 @@ package modeles;
 import java.awt.Color;
 import java.awt.Graphics;
 
+/**
+ * Horaire est une classe qui donne les donnees sur les horaires de la ligne a une station
+ * Elle indique le jour dans la semaine, l'heure et la minute de passage ainsi que la periode
+ * @version 1.0
+ */
 public class Horaire {
 	private int jSemaine;
 	private int heure;
 	private int minute;
 	private int periode;
 	
+	/**
+	 * Constructeur par defaut
+	 */
 	public Horaire(){
 		this.jSemaine = -1;
 		this.heure = -1;
@@ -16,6 +24,13 @@ public class Horaire {
 		this.periode = -1;
 	}
 	
+	/**
+	 * Constructeur prenant en paramtre des entiers indiquants le jour de la semaine, l'heure, la minute et la periode
+	 * @param jSemaine, Un entier qui definit le jour de la semaine (compris entre 1 et 7)
+	 * @param heure, Un entier qui definit l'heure de passage (compris entre 0 et 23)
+	 * @param minute, Un entier qui definit la minute de passage (compris entre 0 et 59)
+	 * @param periode, Un entier qui sert d'identifiant de la période
+	 */
 	public Horaire(int jSemaine, int heure, int minute, int periode){
 		this.jSemaine = jSemaine;
 		this.heure = heure;
@@ -23,6 +38,13 @@ public class Horaire {
 		this.periode = periode;
 	}
 	
+	/**
+	 * Dessine la fenetre avec les horaires
+	 * @param g, Une instance de Graphics
+	 * @param x, Un entier qui correspond a l'abscisse d'origine de la fenetre
+	 * @param y, Un entier qui correspond a l'ordonnee d'origine de la fenetre
+	 * @see Graphics 
+	 */
 	public void dessinerHoraire(Graphics g, int x, int y){
 		int hauteur = 30;
 		int largeur = 90;
@@ -52,26 +74,51 @@ public class Horaire {
 		g.drawString(str, x - l / 2 + largeur / 2, y + h / 4 + hauteur / 2);
 	}
 	
+	/**
+	 * Recupere le jour de la semaine
+	 * @return Un entier qui correspond au jour de la semaine
+	 */
 	public int getJSemaine(){
 		return this.jSemaine;
 	}
 	
+	/**
+	 * Recupere l'heure
+	 * @return Un entier qui correspond a l'heure
+	 */
 	public int getHeure(){
 		return this.heure;
 	}
 	
+	/**
+	 * Recupere la minute
+	 * @return Un entier qui correspond a la minute 
+	 */
 	public int getMinute(){
 		return this.minute;
 	}
 	
+	/**
+	 * Recupere la periode
+	 * @return Un entier qui correspond a la periode
+	 */
 	public int getPeriode(){
 		return this.periode;
 	}
 	
+	/**
+	 * Definit le jour de la semaine
+	 * @param jSemaine, Un entier compris entre 1 et 7
+	 */
 	public void setJSemaine(int jSemaine){
-		this.jSemaine = jSemaine;
+		if(jSemaine < 8 && jSemaine > 0)
+			this.jSemaine = jSemaine;
 	}
 	
+	/**
+	 * Definit l'heure
+	 * @param heure, Un entier compris entre 0 et 23
+	 */
 	public void setHeure(int heure){
 		if(heure >= 24){
 			this.setJSemaine(this.getJSemaine() + this.getHeure() / 24);
@@ -82,6 +129,10 @@ public class Horaire {
 		}
 	}
 	
+	/**
+	 * Definit la minute
+	 * @param minute, Un entier compris entre 0 et 59
+	 */
 	public void setMinute(int minute){
 		if(minute >= 60){
 			this.setHeure(this.getHeure() + this.getMinute() / 60);
@@ -92,10 +143,17 @@ public class Horaire {
 		}
 	}
 	
+	/**
+	 * Definit la periode
+	 * @param periode, Un entier
+	 */
 	public void setPeriode(int periode){
 		this.periode = periode;
 	}
 	
+	/**
+	 * Affiche l'heure, et la minute de l'horaire
+	 */
 	public String toString(){
 		String str = "";
 		str += this.getHeure(); 
--- src/modeles/Ligne.java
+++ src/modeles/Ligne.java
@@ -1,8 +1,15 @@
 package modeles;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Vector;
 
+/**
+ * Ligne est la classe representant une ligne, quelque soit le type de celle-ci
+ * Attention, une ligne réelle est constituée de deux lignes conceptuelles.
+ * 
+ * @version 1.0
+ */
 public class Ligne extends Station{
 	private int id;
 	private String nom;
@@ -10,12 +17,25 @@ public class Ligne extends Station{
 	private MoyenTransport transport;
 	private Vector<Station> listeStations = new Vector<Station>();
 	
+	/**
+	 * Constructeur par defaut d'une ligne
+	 */
 	public Ligne(){
 		this.id = -1;
 		this.nom = "Pas de nom";
 		this.couleur = new Color(100, 100, 100);
 	}
 	
+	/**
+	 * Constructeur d'une ligne avec parametres
+	 * @param _id 		Un entier, un identifiant unique pour la ligne 
+	 * @param _nom 		Une chaine de caracteres, le nom de la ligne
+	 * @param _couleur 	Ue couleur (3 entiers), définit la couleur sur la carte de la ligne, 
+	 * attention, la ligne retour doit possèder la même couleur
+	 * @param transport	Un MoyenTransport, le moyen de transport utilisé sur la ligne (bus, tram etc)
+	 * @see Color
+	 * @see MoyenTransport
+	 */
 	public Ligne(int _id, String _nom, Color _couleur, MoyenTransport transport){
 		this.id = _id;
 		this.nom = _nom;
@@ -23,6 +43,11 @@ public class Ligne extends Station{
 		this.transport = transport;
 	}
 	
+	/**
+	 * Dessine la ligne a l'affichage
+	 * @param g Une instance de Graphics
+	 * @see Graphics
+	 */
 	public void dessinerLigne(Graphics g){
 		for(int i = 0;i < listeStations.size();i++){
 			if(i < listeStations.size() - 1){
@@ -35,6 +60,11 @@ public class Ligne extends Station{
 		}
 	}
 
+	/**
+	 * Insert une station dans la ligne
+	 * @param station	Une Station que l'on inclut dans la ligne
+	 * @param numero	Un entier qui donne l'ordre de la station pour la ligne
+	 */
 	public void insertStation(Station station, int numero) {
 		if (numero < listeStations.size())
 			listeStations.insertElementAt(station, numero);
@@ -42,26 +72,54 @@ public class Ligne extends Station{
 			System.out.println("Erreur d'insertion de station dans la Ligne !");
 	}
 	
+	/**
+	 * Insert une station a la fin de la ligne
+	 * @param station	Une Station qui devient le nouveau terminus
+	 */
 	public void ajouterStation(Station station){
 		listeStations.add(station);
 	}
 	
+	/**
+	 * Donne l'identifiant de la station
+	 * @return Un entier, l'identifiant de la station
+	 */
 	public int getId(){
 		return this.id;
 	}
 	
+	/**
+	 * Donne le nom de la station
+	 * @return Une chaine de caractere, le nom de la ligne
+	 */
 	public String getNom(){
 		return this.nom;
 	}
-	
+
+	/**
+	 * Donne la couleur de la station
+	 * @return Une couleur, la couleur de la ligne
+	 * @see Color
+	 */
 	public Color getCouleur(){
 		return this.couleur;
 	}
 	
+	/**
+	 * Donne le moyen de transport de la ligne
+	 * @return Un MoyenTransport, le moyen de transport employe par la ligne
+	 * @see Moyentransport
+	 */
 	public MoyenTransport getTransport(){
 		return this.transport;
 	}
 	
+	/**
+	 * Retourne la station au numero indique
+	 * @param numero, un entier auquel correspond une station 
+	 * @return Une Station, celle au numero envoye en parametre
+	 * @see Station 
+	 */
 	public Station getStation(int numero) {
 		if (numero < listeStations.size())
 			return listeStations.elementAt(numero);
@@ -71,42 +129,82 @@ public class Ligne extends Station{
 		}
 	}
 	
+	/**
+	 * Retourne l'ordre de la station indiquee dans la ligne
+	 * @param station, une station dont on cherche le numero
+	 * @return un entier, l'ordre de la station envoyee en parametre dans la ligne
+	 * @see Station
+	 */
 	public int getNumeroStation(Station station) {
 		return listeStations.indexOf(station);
 	}
 	
+	/**
+	 * Retourne la liste des stations dans l'ordre de la ligne
+	 * @return Vector<Stations>, la liste des stations de la ligne
+	 * @see Station
+	 */
 	public Vector<Station> getListeStations() {
 		return listeStations;
 	}
 	
+	/**
+	 * Permet de donner un id a la ligne
+	 */
 	public void setId(int _id){
 		this.id = _id;
 	}
 	
+	/**
+	 * Permet de donner un nom a la ligne
+	 */
 	public void setNom(String _nom){
 		this.nom = _nom;
 	}
 
+	/**
+	 * Permet de donner une couleur a la ligne
+	 * @param _couleur
+	 */
 	public void setCouleur(Color _couleur){
 		this.couleur = _couleur;
 	}
 	
+	/**
+	 * Permet de donner un moyen de transport a la ligne
+	 * @param transport
+	 */
 	public void setTransport(MoyenTransport transport){
 		this.transport = transport;
 	}
 	
+	/**
+	 * Definit la station de depart
+	 * @param station
+	 */
 	public void setStationDepart(Station station) {
 		listeStations.add(0, station);
 	}
 
+	/**
+	 * Definit le terminus
+	 * @param station
+	 */
 	public void setStationArrivee(Station station) {
 		listeStations.add(listeStations.size() , station);
 	}
 	
+	/**
+	 * Permet de donner une liste de station a la ligne
+	 * @param listeStations
+	 */
 	public void setListeStations(Vector<Station> listeStations) {
 		this.listeStations = listeStations;
 	}
 	
+	/**
+	 * Affiche l'ID, le nom et les stations de la ligne avec les horaires.
+	 */
 	public String toString() {
 		String str;
 		str = "";
